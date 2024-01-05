4fcb6d19d09f15a5fb5dfec9be14f5bd324fe010
ipfreely-uk <ipfreely.uk@protom.me>
2024-01-05 16:05:43 +0000

    Hello World! commit

---
+++ code/src/test/java/uk/ipfreely/MainTest.java
@@ -0,0 +1,14 @@
+package uk.ipfreely;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        Main.main(new String[0]);
+        assertTrue(true);
+    }
+}
