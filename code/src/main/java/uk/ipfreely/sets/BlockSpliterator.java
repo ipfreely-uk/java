d740d3fed34749292558bd7868555c0ec941c26d
ipfreely-uk <ipfreely.uk@protom.me>
2024-09-27 14:36:10 +0100

    Minor enhancement

--- code/src/main/java/uk/ipfreely/sets/BlockSpliterator.java
+++ code/src/main/java/uk/ipfreely/sets/BlockSpliterator.java
@@ -12,8 +12,8 @@ import static uk.ipfreely.sets.Validation.validate;
 
 final class BlockSpliterator<A extends Address<A>> implements Spliterator<Block<A>> {
 
-    // TODO: can replace with constant
-    private static final double LOG_2 = Math.log(2.0);
+    // Math.log(2.0)
+    private static final double LOG_2 = 0.6931471805599453;
 
     private A start;
     private final A end;
