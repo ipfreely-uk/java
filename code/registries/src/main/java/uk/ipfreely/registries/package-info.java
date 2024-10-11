f173de0a712bdf0104b420d0caefb7c3230f38ed
ipfreely-uk <ipfreely.uk@protom.me>
2024-10-11 15:37:23 +0100

    Documentation

--- code/registries/src/main/java/uk/ipfreely/registries/package-info.java
+++ code/registries/src/main/java/uk/ipfreely/registries/package-info.java
@@ -1,10 +1,13 @@
 /**
- * Data derived from IANA
- * <a href="https://www.iana.org/numbers">IP Address Allocations</a>.
- *
+ * <p>
+ *     Data derived from IANA <a href="https://www.iana.org/numbers">IP Address Allocations</a>.
+ *     This library aims enable more complex behaviour than can be supported with
+ *     methods like {@link java.net.InetAddress#isMulticastAddress()},
+ *     {@link java.net.InetAddress#isSiteLocalAddress()}, etc.
+ * </p>
  * <p>
  *     The module name of this library is "uk.ipfreely.registries".
- *     This module relies on the "java.xml" library.
+ *     This module relies on the "java.xml" module.
  * </p>
  */
 package uk.ipfreely.registries;
