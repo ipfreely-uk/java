f1d179259426d40c144d616e45c3dcf67713c39e
ipfreely-uk <ipfreely.uk@protom.me>
2025-01-06 12:09:33 +0000

    Rewrote IPv4 mask logic

--- code/src/test/java/uk/ipfreely/V4MaskingTest.java
+++ code/src/test/java/uk/ipfreely/V4MaskingTest.java
@@ -10,16 +10,17 @@ class V4MaskingTest {
 
     @Test
     void maskSizeIfBlock() {
-        assertEquals(-1, V4Masking.maskSizeIfBlock(0b1, 0b11));
-        assertEquals(-1, V4Masking.maskSizeIfBlock(0b011, 0b100));
-        assertEquals(-1, V4Masking.maskSizeIfBlock(0b1011, 0b1100));
         assertEquals(0, V4Masking.maskSizeIfBlock(0x0, 0xFFFFFFFF));
         assertEquals(32, V4Masking.maskSizeIfBlock(0x0, 0x0));
         assertEquals(24, V4Masking.maskSizeIfBlock(0xCAFEBB00, 0xCAFEBBFF));
         assertEquals(16, V4Masking.maskSizeIfBlock(0xFFFF0000, 0xFFFFFFFF));
-        assertEquals(-1, V4Masking.maskSizeIfBlock(0xABFF0000, 0xFFFFFFFF));
         assertEquals(32, V4Masking.maskSizeIfBlock(0xFFFFFFFF, 0xFFFFFFFF));
+
+        assertEquals(-1, V4Masking.maskSizeIfBlock(0xABFF0000, 0xFFFFFFFF));
         assertEquals(-1, V4Masking.maskSizeIfBlock(0xFFFF0000, 0xFFFE0000));
         assertEquals(-1, V4Masking.maskSizeIfBlock(0xFFFFFFFF, 0xFFFE0000));
+        assertEquals(-1, V4Masking.maskSizeIfBlock(0b1, 0b11));
+        assertEquals(-1, V4Masking.maskSizeIfBlock(0b011, 0b100));
+        assertEquals(-1, V4Masking.maskSizeIfBlock(0b1011, 0b1100));
     }
 }
