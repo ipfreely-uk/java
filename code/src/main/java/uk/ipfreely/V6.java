b78dbe6e810d921c345266ad6585577146593326
ipfreely-uk <ipfreely.uk@protom.me>
2025-07-14 21:02:18 +0100

    Fix imports

--- code/src/main/java/uk/ipfreely/V6.java
+++ code/src/main/java/uk/ipfreely/V6.java
@@ -4,8 +4,6 @@ package uk.ipfreely;
 
 import java.math.BigInteger;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.stream.IntStream;
 
 import static java.util.Arrays.asList;
